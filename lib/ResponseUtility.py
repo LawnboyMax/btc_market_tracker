67303fceacc882df13b8546227e4a1d6f2e96a52
Maxim Mikhaylov <lawnboymadmax@gmail.com>
2017-04-13 19:04:25 -0400

    Class is now 500% shorter
    Removed lots of redundancy

--- lib/ResponseUtility.py
+++ lib/ResponseUtility.py
@@ -1,37 +1,53 @@
 import time
+import re
 
 # Methods that extract and format stats from exchange responses to API requests
-
 class ResponseUtility:
     SIG_DIG = 2
 
     # Selects method specific to chosen exchange and passes on the response
     @staticmethod
-    def select_exchange(response, exchange, watchlist):
+    def getInfo(response, exchange, watchlist):
 
         ResponseUtility.exchange = exchange
-        if exchange == 'bitfinex':
-            return ResponseUtility().bitfinex_prices(response, watchlist)
-        elif exchange == 'kraken':
-            return ResponseUtility().kraken_prices(response, watchlist)
-        elif exchange == 'quadrigacx':
-            return ResponseUtility().quadriga_prices(response, watchlist)
-        elif exchange == 'taurus':
-            return ResponseUtility().taurus_prices(response, watchlist)
-        elif exchange == 'btc-e':
-            return ResponseUtility().btce_prices(response, watchlist)
-        elif exchange == 'bitstamp':
-            return ResponseUtility().bitstamp_prices(response, watchlist)
-        elif exchange == 'okcoin':
-            return ResponseUtility().okcoin_prices(response, watchlist)
-        elif exchange == 'anxpro':
-            return ResponseUtility().anxpro_prices(response, watchlist)
-        elif exchange == 'hitbtc':
-            return ResponseUtility().hitbtc_prices(response, watchlist)
+        price_list = []
+        response = response.replace('\n', ' ').replace('\r', '') #remove linebreaks for easier regex matching
+
+        for field in watchlist:
+            matchnum = 1
+            # accounts for aliases used by different exchanges
+            if field == 'buy':
+                field = '(ask|buy|\Wa|buy.+?value)'
+                matchnum = 2
+            elif field == 'sell':
+                field = '(bid|sell|\Wb|sell.+?value)'
+                matchnum = 2
+            elif field == 'vwap':
+                field = '(vwap|\Wp|vwap.+?value)'
+                matchnum = 2
+            elif field == 'low':
+                field = '(low|\Wl|low.+?value)'
+                matchnum = 2
+            elif field == 'high':
+                field = '(high|\Wh|high.+?value)'
+                matchnum = 2
+
+            ex = field + '\W\W\W??\W??(\d*\.?\d+)' # it is possible that there are 2-4 characters between field name and value
+            regex = re.compile(ex, re.IGNORECASE)
+            try:
+                match = regex.search(response)
+                price_list.append(float(match.group(matchnum)))
+            except AttributeError:
+                price_list.append(0) # field does not exits. Accounts for lack of WVAP in most cases
+
+        price_list_rounded = ResponseUtility().roundPrices(price_list) # Round prices
+        price_list_rounded = [x if (x != '0') else 'N/A' for x in price_list_rounded] # print N/A for field which is 0
+        return price_list_rounded
 
     # Rounds floats to SIG_DIG significant digits and return as str list. First element of list is exchange name.
     @staticmethod
-    def round_prices(price_list):
+    def roundPrices(price_list):
+
         price_list_rounded = []
         price_list_rounded.append(ResponseUtility.exchange.capitalize()) # add exchange name for use with tabulate library
         for p in price_list:
@@ -39,270 +55,3 @@ class ResponseUtility:
             p = str(p)
             price_list_rounded.append(p)
         return price_list_rounded
-
-    # Extract prices from Bitfinex response string
-    @staticmethod
-    def bitfinex_prices(response, watchlist):
-        # Start index is inclusive, end index is exclusive
-        low_index_start = response.find('low') + 6
-        low_index_end = low_index_start + response[low_index_start:].find('\",\"')
-        high_index_start = response.find('high') + 7
-        high_index_end = high_index_start + response[high_index_start:].find('\",\"')
-        buy_index_start = response.find('ask') + 6
-        buy_index_end = buy_index_start + response[buy_index_start:].find('\",\"')
-        sell_index_start = response.find('bid') + 6
-        sell_index_end = sell_index_start + response[sell_index_start:].find('\",\"')
-        # Convert prices to float
-        price_list = []
-        if 'vwap' in watchlist:
-            price_list.append(0)
-        if 'low' in watchlist:
-           price_list.append(float(response[low_index_start : low_index_end]))
-        if 'high' in watchlist:
-            price_list.append(float(response[high_index_start : high_index_end]))
-        if 'buy' in watchlist:
-           price_list.append(float(response[buy_index_start : buy_index_end]))
-        if 'sell' in watchlist:
-            price_list.append(float(response[sell_index_start : sell_index_end]))
-        price_list_rounded = ResponseUtility().round_prices(price_list) # Round prices
-        price_list_rounded[1] = "N/A" # VWAP not available
-        return price_list_rounded
-
-    # Extract prices from Kraken response string
-    @staticmethod
-    def kraken_prices(response, watchlist):
-        # Start index is inclusive, end index is exclusive
-        vwap = response.find('\"p\"')
-        vwap_index_start = vwap + response[vwap:].find('\",\"') + 3
-        vwap_index_end = vwap_index_start + response[vwap_index_start:].find('\"]')
-        low = response.find('\"l\"')
-        low_index_start = low + response[low:].find('\",\"') + 3
-        low_index_end = low_index_start + response[low_index_start:].find('\"]')
-        high = response.find('\"h\"')
-        high_index_start = high + response[high:].find('\",\"') + 3
-        high_index_end = high_index_start + response[high_index_start:].find('\"]')
-        buy_index_start = response.find('\"a\"') + 6
-        buy_index_end = buy_index_start + response[buy_index_start:].find('\",\"')
-        sell_index_start = response.find('\"b\"') + 6
-        sell_index_end = sell_index_start + response[sell_index_start:].find('\",\"')
-        # Convert prices to float
-        price_list = []
-        if 'vwap' in watchlist:
-            price_list.append(float(response[vwap_index_start : vwap_index_end]))
-        if 'low' in watchlist:
-           price_list.append(float(response[low_index_start : low_index_end]))
-        if 'high' in watchlist:
-            price_list.append(float(response[high_index_start : high_index_end]))
-        if 'buy' in watchlist:
-           price_list.append(float(response[buy_index_start : buy_index_end]))
-        if 'sell' in watchlist:
-            price_list.append(float(response[sell_index_start : sell_index_end]))
-        price_list_rounded = ResponseUtility().round_prices(price_list) # Round prices
-        return price_list_rounded
-
-    # Extract prices from Quadriga response string
-    @staticmethod
-    def quadriga_prices(response, watchlist):
-        # Start index is inclusive, end index is exclusive
-        vwap_index_start = response.find('vwap') + 7
-        vwap_index_end = vwap_index_start + response[vwap_index_start:].find('\",\"')
-        low_index_start = response.find('low') + 6
-        low_index_end = low_index_start + response[low_index_start:].find('\",\"')
-        high_index_start = response.find('high') + 7
-        high_index_end = high_index_start + response[high_index_start:].find('\",\"')
-        buy_index_start = response.find('ask') + 6
-        buy_index_end = buy_index_start + response[buy_index_start:].find('\",\"')
-        sell_index_start = response.find('bid') + 6
-        sell_index_end = sell_index_start + response[sell_index_start:].find('\"}')
-        # Convert prices to float
-        price_list = []
-        if 'vwap' in watchlist:
-            price_list.append(float(response[vwap_index_start : vwap_index_end]))
-        if 'low' in watchlist:
-           price_list.append(float(response[low_index_start : low_index_end]))
-        if 'high' in watchlist:
-            price_list.append(float(response[high_index_start : high_index_end]))
-        if 'buy' in watchlist:
-           price_list.append(float(response[buy_index_start : buy_index_end]))
-        if 'sell' in watchlist:
-            price_list.append(float(response[sell_index_start : sell_index_end]))
-        price_list_rounded = ResponseUtility().round_prices(price_list) # Round prices
-        return price_list_rounded
-
-    # Extract prices from Taurus response string
-    @staticmethod
-    def taurus_prices(response, watchlist):
-        # Start index is inclusive, end index is exclusive
-        vwap_index_start = response.find('vwap') + 7
-        vwap_index_end = vwap_index_start + response[vwap_index_start:].find('\",\"')
-        low_index_start = response.find('low') + 6
-        low_index_end = low_index_start + response[low_index_start:].find('\",\"')
-        high_index_start = response.find('high') + 7
-        high_index_end = high_index_start + response[high_index_start:].find('\",\"')
-        buy_index_start = response.find('ask') + 6
-        buy_index_end = buy_index_start + response[buy_index_start:].find('\",\"')
-        sell_index_start = response.find('bid') + 6
-        sell_index_end = sell_index_start + response[sell_index_start:].find('\",\"')
-        # Convert prices to float
-        price_list = []
-        if 'vwap' in watchlist:
-            price_list.append(float(response[vwap_index_start : vwap_index_end]))
-        if 'low' in watchlist:
-           price_list.append(float(response[low_index_start : low_index_end]))
-        if 'high' in watchlist:
-            price_list.append(float(response[high_index_start : high_index_end]))
-        if 'buy' in watchlist:
-           price_list.append(float(response[buy_index_start : buy_index_end]))
-        if 'sell' in watchlist:
-            price_list.append(float(response[sell_index_start : sell_index_end]))
-        price_list_rounded = ResponseUtility().round_prices(price_list) # Round prices
-        return price_list_rounded
-
-    # Extract prices from Taurus response string
-    @staticmethod
-    def btce_prices(response, watchlist):
-        # Start index is inclusive, end index is exclusive
-        low_index_start = response.find('low') + 5
-        low_index_end = low_index_start + response[low_index_start:].find(',\"')
-        high_index_start = response.find('high') + 6
-        high_index_end = high_index_start + response[high_index_start:].find(',\"')
-        buy_index_start = response.find('buy') + 5
-        buy_index_end = buy_index_start + response[buy_index_start:].find(',\"')
-        sell_index_start = response.find('sell') + 6
-        sell_index_end = sell_index_start + response[sell_index_start:].find(',\"')
-        # Convert prices to float
-        price_list = []
-        if 'vwap' in watchlist:
-            price_list.append(0)
-        if 'low' in watchlist:
-           price_list.append(float(response[low_index_start : low_index_end]))
-        if 'high' in watchlist:
-            price_list.append(float(response[high_index_start : high_index_end]))
-        if 'buy' in watchlist:
-           price_list.append(float(response[buy_index_start : buy_index_end]))
-        if 'sell' in watchlist:
-            price_list.append(float(response[sell_index_start : sell_index_end]))
-        price_list_rounded = ResponseUtility().round_prices(price_list) # Round prices
-        price_list_rounded[1] = "N/A" # VWAP not available
-        return price_list_rounded
-
-     # Extract prices from Bitstamp response string
-    @staticmethod
-    def bitstamp_prices(response, watchlist):
-        # Start index is inclusive, end index is exclusive
-        vwap_index_start = response.find('vwap') + 8
-        vwap_index_end = vwap_index_start + response[vwap_index_start:].find('\", \"')
-        low_index_start = response.find('low') + 7
-        low_index_end = low_index_start + response[low_index_start:].find('\", \"')
-        high_index_start = response.find('high') + 8
-        high_index_end = high_index_start + response[high_index_start:].find('\", \"')
-        buy_index_start = response.find('ask') + 7
-        buy_index_end = buy_index_start + response[buy_index_start:].find('\", \"')
-        sell_index_start = response.find('bid') + 7
-        sell_index_end = sell_index_start + response[sell_index_start:].find('\", \"')
-        # Convert prices to float
-        price_list = []
-        if 'vwap' in watchlist:
-            price_list.append(float(response[vwap_index_start : vwap_index_end]))
-        if 'low' in watchlist:
-           price_list.append(float(response[low_index_start : low_index_end]))
-        if 'high' in watchlist:
-            price_list.append(float(response[high_index_start : high_index_end]))
-        if 'buy' in watchlist:
-           price_list.append(float(response[buy_index_start : buy_index_end]))
-        if 'sell' in watchlist:
-            price_list.append(float(response[sell_index_start : sell_index_end]))
-        price_list_rounded = ResponseUtility().round_prices(price_list) # Round prices
-        return price_list_rounded
-
-     # Extract prices from OKCoin response string
-    @staticmethod
-    def okcoin_prices(response, watchlist):
-        # Start index is inclusive, end index is exclusive
-        low_index_start = response.find('low') + 6
-        low_index_end = low_index_start + response[low_index_start:].find('\",\"')
-        high_index_start = response.find('high') + 7
-        high_index_end = high_index_start + response[high_index_start:].find('\",\"')
-        buy_index_start = response.find('buy') + 6
-        buy_index_end = buy_index_start + response[buy_index_start:].find('\",\"')
-        sell_index_start = response.find('sell') + 7
-        sell_index_end = sell_index_start + response[sell_index_start:].find('\",\"')
-         # Convert prices to float
-        price_list = []
-        if 'vwap' in watchlist:
-            price_list.append(0)
-        if 'low' in watchlist:
-           price_list.append(float(response[low_index_start : low_index_end]))
-        if 'high' in watchlist:
-            price_list.append(float(response[high_index_start : high_index_end]))
-        if 'buy' in watchlist:
-           price_list.append(float(response[buy_index_start : buy_index_end]))
-        if 'sell' in watchlist:
-            price_list.append(float(response[sell_index_start : sell_index_end]))
-        price_list_rounded = ResponseUtility().round_prices(price_list) # Round prices
-        price_list_rounded[1] = "N/A" # VWAP not available
-        return price_list_rounded
-
-    # Extract prices from ANXPRO response string
-    @staticmethod
-    def anxpro_prices(response, watchlist):
-        # Start index is inclusive, end index is exclusive
-        vwap = response.find('\"vwap\"')
-        vwap_index_start = vwap + response[vwap:].find('\"value\":') + 10
-        vwap_index_end = vwap_index_start + response[vwap_index_start:].find('\"')
-        low = response.find('\"low\"')
-        low_index_start = low + response[low:].find('\"value\":') + 10
-        low_index_end = low_index_start + response[low_index_start:].find('\"')
-        high = response.find('\"high\"')
-        high_index_start = high + response[high:].find('\"value\":') + 10
-        high_index_end = high_index_start + response[high_index_start:].find('\"')
-        buy = response.find('\"buy\"')
-        buy_index_start = buy + response[buy:].find('\"value\":') + 10
-        buy_index_end = buy_index_start + response[buy_index_start:].find('\"')
-        sell = response.find('\"sell\"')
-        sell_index_start = sell + response[sell:].find('\"value\":') + 10
-        sell_index_end = sell_index_start + response[sell_index_start:].find('\"')
-        # Convert prices to float
-        price_list = []
-        if 'vwap' in watchlist:
-            price_list.append(float(response[vwap_index_start : vwap_index_end]))
-        if 'low' in watchlist:
-           price_list.append(float(response[low_index_start : low_index_end]))
-        if 'high' in watchlist:
-            price_list.append(float(response[high_index_start : high_index_end]))
-        if 'buy' in watchlist:
-           price_list.append(float(response[buy_index_start : buy_index_end]))
-        if 'sell' in watchlist:
-            price_list.append(float(response[sell_index_start : sell_index_end]))
-        price_list_rounded = ResponseUtility().round_prices(price_list) # Round prices
-        return price_list_rounded
-
-    # Extract prices from HitBTC response string
-    @staticmethod
-    def hitbtc_prices(response, watchlist):
-        # Start index is inclusive, end index is exclusive
-        low_index_start = response.find('low') + 6
-        low_index_end = low_index_start + response[low_index_start:].find('\",\"')
-        high_index_start = response.find('high') + 7
-        high_index_end = high_index_start + response[high_index_start:].find('\",\"')
-        buy_index_start = response.find('ask') + 6
-        buy_index_end = buy_index_start + response[buy_index_start:].find('\",\"')
-        sell_index_start = response.find('bid') + 6
-        sell_index_end = sell_index_start + response[sell_index_start:].find('\",\"')
-         # Convert prices to float
-        price_list = []
-        if 'vwap' in watchlist:
-            price_list.append(0)
-        if 'low' in watchlist:
-           price_list.append(float(response[low_index_start : low_index_end]))
-        if 'high' in watchlist:
-            price_list.append(float(response[high_index_start : high_index_end]))
-        if 'buy' in watchlist:
-           price_list.append(float(response[buy_index_start : buy_index_end]))
-        if 'sell' in watchlist:
-            price_list.append(float(response[sell_index_start : sell_index_end]))
-        price_list_rounded = ResponseUtility().round_prices(price_list) # Round prices
-        price_list_rounded[1] = "N/A" # VWAP not available
-        return price_list_rounded
-    
-
